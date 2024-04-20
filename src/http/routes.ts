86ba1d10f13cdba9c35a849ded23d3ea77f8f033
israelcruzz <israelcruz.contato@gmail.com>
2024-04-20 18:49:56 -0300

    feat: route flow creation

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,7 +1,18 @@
-import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { FastifyInstance } from "fastify";
 
 export default async function AppRoutes(app: FastifyInstance) {
-    app.get('/', (request: FastifyRequest, reply: FastifyReply) => {
-        reply.send({ message: 'server online' })
-    })
+    app.post('/auth', () => {})
+    app.get('/user/ads', () => {})
+    app.delete('/user/delete/:adId', () => {})
+    
+    app.post('/ad', () => {})
+    app.get('/ad/:gameId', () => {})
+    app.get('/ad/recents', () => {})
+    app.get('/discord/adId', () => {})
+
+    app.get('/games', () => {})
+    app.get('/famous/game', () => {})
+    app.get('/game/:categoryId', () => {})
+
+    app.get('/category', () => {})
 }
