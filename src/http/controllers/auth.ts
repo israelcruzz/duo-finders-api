65a0996e89f2703863e6b9288889163ab6948ea4
israelcruzz <israelcruz.contato@gmail.com>
2024-04-20 21:21:14 -0300

    Controllers were created for authentication, listing a user's ads and deleting an ad

---
+++ src/http/controllers/auth.ts
@@ -0,0 +1,5 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export async function Auth(request: FastifyRequest, reply: FastifyReply) {
+
+}
---
+++ src/http/controllers/delete-ad.ts
@@ -0,0 +1,5 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export async function DeleteAd(request: FastifyRequest, reply: FastifyReply) {
+
+}
