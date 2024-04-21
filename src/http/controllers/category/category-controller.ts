f195b6e6f135e1c6f8f38a72f2abd34db0eee0db
israelcruzz <israelcruz.contato@gmail.com>
2024-04-20 23:31:45 -0300

    feat: was created controller to category

--- src/http/controllers/category/category-controller.ts
+++ src/http/controllers/category/category-controller.ts
@@ -1,5 +1,20 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { FindManyCategoriesUseCase } from "../../../use-cases/category/find-many-categories";
+import categoryInMemoryRepositorie from "../../../repositories/category/in-memory/category-in-memory-repositorie";
 
-export async function CategoryController(request: FastifyRequest, reply: FastifyReply) {
+export async function CategoryController(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const findManyCategories = new FindManyCategoriesUseCase(
+    categoryInMemoryRepositorie
+  );
 
+  try {
+    const categories = await findManyCategories.execute();
+
+    reply.code(201).send(categories);
+  } catch (error) {
+    reply.code(404).send({ message: "Internal Server Error" });
+  }
 }
