bf2e081524f485f1f999fd29a6838aaa5b4c8ef0
israelcruzz <israelcruz.contato@gmail.com>
2024-04-21 21:21:01 -0300

    test: create ad use case test

---
+++ src/use-cases/ad/tests/create-ad-use-case.spec.ts
@@ -0,0 +1,27 @@
+import { describe, it, expect, beforeAll, afterAll } from "vitest";
+import adInMemoryRepositorie from "../../../repositories/ad/in-memory/ad-in-memory-repositorie";
+import { CreateAdUseCase } from "../create-ad-use-case";
+
+describe("Create Ad Use Case", () => {
+  afterAll(() => {
+    adInMemoryRepositorie.ads = [];
+  });
+
+  it("should be able to create ad", async () => {
+    const createGame = new CreateAdUseCase(adInMemoryRepositorie);
+
+    const ad = await createGame.execute({
+      name: "test",
+      discord: "test",
+      gameId: "test",
+      hoursEnd: 2,
+      hoursStart: 2,
+      userId: "test",
+      useVoiceChannel: true,
+      weekDays: "sun",
+      yearPlaying: 2,
+    });
+
+    expect(ad.name).toEqual(expect.any(String));
+  });
+});
