20bf9db37d461de6986ddb0919b0db8b563a3f69
israelcruzz <israelcruz.contato@gmail.com>
2024-04-21 21:00:18 -0300

    test: implemented get discord ad use case test

---
+++ src/use-cases/ad/tests/get-discord-ad-use-case.spec.ts
@@ -0,0 +1,42 @@
+import { describe, it, expect, beforeAll, afterAll, vi } from "vitest";
+import adInMemoryRepositorie from "../../../repositories/ad/in-memory/ad-in-memory-repositorie";
+import { GetGameAdsUseCase } from "../get-game-ads-use-case";
+import { GetDiscordAdUseCase } from "../get-discord-ad-use-case";
+
+describe("Get Discord Ad Use Case", () => {
+  beforeAll(() => {
+    adInMemoryRepositorie.ads.push({
+      id: "ad-test",
+      name: "user-test",
+      discord: "user-test",
+      gameId: "user-test",
+      hoursEnd: 1,
+      hoursStart: 2,
+      userId: "user-test",
+      useVoiceChannel: true,
+      weekDays: "user-test",
+      yearPlaying: 4,
+      createdAt: new Date(2024, 3, 14, 8, 0, 0),
+    });
+  });
+
+  afterAll(() => {
+    adInMemoryRepositorie.ads = [];
+  });
+
+  it("should be able to listing discord ad", async () => {
+    const getDiscordAd = new GetDiscordAdUseCase(adInMemoryRepositorie);
+
+    const discordName = await getDiscordAd.execute({ adId: "ad-test" });
+
+    expect(discordName).toEqual(expect.any(String));
+  });
+
+  it("should not be able to listing discord ad", async () => {
+    const getDiscordAd = new GetDiscordAdUseCase(adInMemoryRepositorie);
+
+    const discordName = await getDiscordAd.execute({ adId: "not-exist-test" });
+
+    expect(discordName).toBeUndefined
+  });
+});
